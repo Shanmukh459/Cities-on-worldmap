30a389819d1383d54745b58136a2e6401ac0787c
Shanmukh459 <saikrrish459@gmail.com>
2024-05-06 13:07:27 -0700

    loading worldatlas data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react"
+import { json } from "d3"
+import { feature } from "topojson"
+
+const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
+
 function App() {
-  return <h1>Cities on WorldMap!</h1>
+  const [worldAtlas, setWorldAtlas] = useState()
+
+  useEffect(() => {
+    json(jsonUrl).then(topology => {
+      const { countries } = topology.objects
+      setWorldAtlas(feature(topology, countries))
+    })
+  }, [])
+  
+  if(!worldAtlas) {
+    return <h1>Loading...</h1>
+  }
+
+  console.log(worldAtlas)
+  
+  return (
+    <h1>Hello!</h1>
+  )
 }
 
 export default App
